f5857c0501c22d17699fe6ea3639ba631e4661fb
Ingo Karkat <swdev@ingo-karkat.de>
2021-01-05 16:04:43 +0100

    Refactoring: Introduce projectDir constant

--- check/smoketest.sh
+++ check/smoketest.sh
@@ -2,5 +2,6 @@
 
 readonly scriptDir="$([ "${BASH_SOURCE[0]}" ] && dirname -- "${BASH_SOURCE[0]}" || exit 3)"
 [ -d "$scriptDir" ] || { echo >&2 "ERROR: Cannot determine script directory!"; exit 3; }
+readonly projectDir="${scriptDir}/.."
 
-"${scriptDir}/../bin/reldate" --help >/dev/null 2>&1
+"${projectDir}/bin/reldate" --help >/dev/null 2>&1
